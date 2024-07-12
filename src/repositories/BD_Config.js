306a57fda987e48db8c2efc2a20e6bbd2b24a6b9
tomasKalinscky <128733997+tomasKalinscky@users.noreply.github.com>
2024-07-12 09:55:27 -0300

    Add files via upload

---
+++ src/repositories/BD_Config.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+export const Bd_config = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+};
